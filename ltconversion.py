52dd40a8748090868500545ed359344409193737
Andrzej1955 <78909468+Andrzej1955@users.noreply.github.com>
2021-03-01 14:55:10 +0100

    Add files via upload

---
+++ ltconversion.py
@@ -0,0 +1,38 @@
+# liczba_tekst_okieno
+# Tworzy okno do wpisania kwoty cyfrowo i uzyskania tekstu tej kwoty
+# Konwersja liczby na format księgowy
+
+
+def conversionToTheAccountingFormat(trojki):
+    '''Zamiana wpowadzonej liczby na format księgowy ###.###.###.##0,00'''
+      
+    kwota_F = ''
+    for s in range(5):
+        kwota_F += trojki[s]+ '.'
+  
+    kwota_F = kwota_F[:19]
+    if kwota_F[:17] == '000.000.000.000.0':
+        kwota_F = '0.' + kwota_F[17:19]
+    else:
+        kwota_F1 = []
+        kwota_F1 = [(kwota_F[i]) for i in range(19)]
+
+        kwota_F = kwota_F1[:]
+        m = 0
+        while kwota_F[m] == '0' or kwota_F[m] == '.':
+            m = 0
+            del kwota_F[m]
+        kwota_F = kwota_F[m:19]
+
+        m=0
+        u = ''
+        d = len(kwota_F)
+        while kwota_F[m]:
+            u += kwota_F[m]
+            m += 1
+            if m == d:
+                break
+        u = u[:-4]+','+u[-2:]
+        kwota_F = u
+
+    return kwota_F
---
+++ ltwrite.py
@@ -0,0 +1,50 @@
+# liczba_tekst_okieno
+# Tworzy okno do wpisania kwoty cyfrowo i uzyskania tekstu tej kwoty
+# Wysietlanie i zapis do pliku tekstu kwoty
+
+from tkinter import *
+from src.libs import openFile
+
+#def amountInWordsOnTheScreen(kwota_s, liczba_P, kwota_F):
+#    '''Wpisanie kwoty słownie na ekranie'''
+#    first = '0'
+#    last = len(liczba_P)
+#    liczba_P.delete(first,last)
+
+#    napis_1 = 'Kwota podana:   '
+#    napis_2 = ' zł\t\t\t\t\t\t\t       Kwota w formacie księgowym:   '
+#    napis_3 = ' zł\nKwota słownie:   '
+#    napis_4 = '\n=====================\n'
+#    napis_5 = 'Kwota słownie została zapisana do pliku "kwota_slownie.txt"\n'
+#    napis = napis_1 + liczba_P + napis_2 + kwota_F + napis_3 + kwota_s + napis_4 + napis_5
+
+#    self.kwota_txt.delete(0.0, END)
+#    self.kwota_txt.insert(0.0, napis)
+#    self.format_gr.set(None)
+
+def checkingOfTheFile():
+    '''Sprawdzenie czy istnieje plik kwota_slownie.txt,
+       utworzenie go gdy nie istnieje a gdy istnieje sparwdzenie jego
+       wielkosci - usunięcie początkowych linii gy jest ich ponad 50'''
+
+    linie = []
+    try:
+        with openFile("kwota_slownie.txt", "r") as lin:  # odczytywanie linia po linii do listy
+            linie = lin.readlines()
+    except (UnicodeDecodeError, FileNotFoundError) :
+        with openFile("kwota_slownie.txt", "w") as lin:
+            lin.close()
+    if len(linie) > 50:
+        del linie[:10]
+    with openFile("kwota_slownie.txt", "w") as lin:  # zapis linii
+        lin.writelines(linie[:])
+
+def enterTheAmountInWords(kwota_s, kwota_C):
+    '''Wpisanie kwoty słownie do pliku 'kwota_slownie.txt'''
+
+    checkingOfTheFile() #Sprawdzenie czy istnieje plik 'kwota_slownie.txt'
+    with openFile("kwota_slownie.txt", "a") as plik:
+        kwoty = ('\nKwota podana księgowo:\n',kwota_C,' zł','\nKwota słownie:\n',kwota_s,'\n')
+        plik.writelines(kwoty)
+
+
